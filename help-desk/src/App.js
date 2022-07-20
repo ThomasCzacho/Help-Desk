e5911378bf6d90bd86afe12d9e5ad669f056eda2
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-07-20 16:21:16 +0000

    HelpDesk

---
+++ help-desk/src/App.js
@@ -0,0 +1,12 @@
+import SignUp from './Components/Signup';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp></SignUp>
+    </div>
+  );
+}
+
+export default App;
